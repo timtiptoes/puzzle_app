e8aa8bd94165fb5c7da842469783d211a0f4ad41
Tim Levine <timtiptoes@yahoo.com>
2018-01-13 16:47:44 -0800

    added puzzle types to radio field

--- model.py
+++ model.py
@@ -2,7 +2,11 @@ from wtforms import Form, FloatField, TextField, RadioField, validators
 
 class InputForm(Form):
     clue = TextField(validators=[validators.InputRequired()])
-    puzzle_type=RadioField('Label', choices=[('Simple_addition','3+5'),('Decode','4 x 3')])
+    puzzle_type=RadioField('Label', choices=[\
+    	('Simple_addition','3+5'),\
+    	('Decode','Simple multiplication and then addition e.g. 4x3+2'),\
+    	('Fraction_addition','Fraction addition e.g. 2/3 + 3/8'),\
+    	('Quadratic equations','Quadratic equation')])
 
 '''
 _problems_map = {"Quadratic equations" : make_quadratic_eq,
