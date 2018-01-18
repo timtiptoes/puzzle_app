eb02a19a6feb067e7a804c8145e322576d75d443
timtiptoes <timtiptoes@yahoo.com>
2018-01-17 22:15:43 -0800

    added examples of each problem;improved view

--- model.py
+++ model.py
@@ -1,14 +1,14 @@
 from wtforms import Form, FloatField, TextField, RadioField, validators
 
 class InputForm(Form):
-    clue = TextField(validators=[validators.InputRequired()])
+    clue = TextField(validators=[validators.InputRequired()],default="I hold legos")
     puzzle_type=RadioField('Label', choices=[
-    	('simple_addition','Simple addition e.g. 3+5'),
-        ('simple_division_problem','Fraction simplification'),
-    	('multiplication_then_addition','Simple multiplication and then addition e.g. 4x3+2'),
-        ('simplify_ratio','Simplify ratio'),
-    	('fraction_addition','Fraction addition e.g. 2/3 + 3/8'),
-        ('quadratic_equations','Quadratic equations')])
+    	('simple_addition','$$\\texttt{Simple addition: }3+5$$'),
+      ('simple_division_problem','$$\\texttt{Simple division: }36\\div3$$'),
+    	('multiplication_then_addition','$$\\texttt{multiplication and addition: } 3\\times5 + 3$$'),
+      ('simplify_ratio','$$\\texttt{use numerator of reduced fraction: }\\frac{11}{65} + \\frac{1}{5}$$'),
+    	('fraction_addition','$$\\texttt{use numerator of sum: }\\frac{2}{7}-\\frac{3}{14}$$'),
+      ('quadratic_equations','$$\\texttt{add roots :}x^2-14x+45$$')])
 
 '''
 To add a new problem type:
