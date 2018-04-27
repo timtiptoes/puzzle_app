5a9f4314ba64b2cebd7b0eb80c60077e56a48522
timtiptoes <timtiptoes@yahoo.com>
2018-04-26 21:33:42 -0700

    iterated on getting in vocab

--- puzzlegen/lib/docparts.py
+++ puzzlegen/lib/docparts.py
@@ -8,8 +8,74 @@ def doc_parts(title="", author=""):
     \def\columnseprulecolor{\color{white}} %Separator ruler colour"""
     if title:
         start = start + "\\title{%s} \n \date{\\vspace{-5ex}} \n \maketitle" % title
+ 
+        end="""
+        \end{document}
+        """
+
+    return start, end
+
+def puzzle_parts(title="", author=""):
+    start="""
+    \\documentclass[16pt]{article}
+    \\usepackage[a4paper,margin=0.5in,portrait]{geometry}
+    \\usepackage{amsmath}
+    \\usepackage{graphicx}
+    \\usepackage{fancyhdr}
+    \\begin{document}
+    \\pagenumbering{gobble}
+    \\includegraphics[height=8cm]{static/code_key.png}
+    """
+
+    end = """\end{document}"""
+    return start, end
+
+def puzzle_section_parts(title, instr="", cols = 2):
+    section_start="""
+        \section*{%s}
+        {\\renewcommand{\\arraystretch}{4}}\\vspace{10mm}
+        \\begin{tabular}{c c c c c c c c c c}
+        """% (instr)
+    section_end = """
+         \\end{tabular}"""
+    return section_start, section_end
+
+def problem(instructions, problem, solution, points=1):
+    code = """
+    \\question[%s]
+        %s
+        %s
+    \\begin{solution} 
+        %s
+    \\end{solution}
+    """ % (str(points), instructions, problem, solution)
+    return code
+
+def puzzle_problem(problem):
+    code = """
+    $\\begin{aligned}[c]
+        %s
+    \\end{aligned}$
+    """ % (problem)
+    return code
+
+def crossword_parts(title="", author=""):
+    start="""
+    \\documentclass{article}
+    \\usepackage[letterpaper, landscape, margin=0.5in]{geometry}
+    \\usepackage{cwpuzzle}
+    \\begin{document}
+    \\pagenumbering{gobble}
+    \\textsc{\\LARGE \centering """
+    start+=title
+    start+=" }"+"\\"+"\\"+"[1.5cm]"
+#    start+="\\"+"\\"+"\\"
 
 
+    middle="""
+    \\end{Puzzle}
+
+    \\begin{PuzzleClues}{\\textbf{Hints}}\\\\"""
     if title=="Phantom Tollbooth":
         end="""
 \begin{multicols}{5}
@@ -240,76 +306,10 @@ wormwood
 
 wrath
 \end{multicols}
-\end{document}"""
-    else:    
-        end="""
-        \end{document}
-        """
+\\end{PuzzleClues}
 
-    return start, end
-
-def puzzle_parts(title="", author=""):
-    start="""
-    \\documentclass[16pt]{article}
-    \\usepackage[a4paper,margin=0.5in,portrait]{geometry}
-    \\usepackage{amsmath}
-    \\usepackage{graphicx}
-    \\usepackage{fancyhdr}
-    \\begin{document}
-    \\pagenumbering{gobble}
-    \\includegraphics[height=8cm]{static/code_key.png}
-    """
-
-    end = """\end{document}"""
-    return start, end
-
-def puzzle_section_parts(title, instr="", cols = 2):
-    section_start="""
-        \section*{%s}
-        {\\renewcommand{\\arraystretch}{4}}\\vspace{10mm}
-        \\begin{tabular}{c c c c c c c c c c}
-        """% (instr)
-    section_end = """
-         \\end{tabular}"""
-    return section_start, section_end
-
-def problem(instructions, problem, solution, points=1):
-    code = """
-    \\question[%s]
-        %s
-        %s
-    \\begin{solution} 
-        %s
-    \\end{solution}
-    """ % (str(points), instructions, problem, solution)
-    return code
-
-def puzzle_problem(problem):
-    code = """
-    $\\begin{aligned}[c]
-        %s
-    \\end{aligned}$
-    """ % (problem)
-    return code
-
-def crossword_parts(title="", author=""):
-    start="""
-    \\documentclass{article}
-    \\usepackage[letterpaper, landscape, margin=0.5in]{geometry}
-    \\usepackage{cwpuzzle}
-    \\begin{document}
-    \\pagenumbering{gobble}
-    \\textsc{\\LARGE \centering """
-    start+=title
-    start+=" }"+"\\"+"\\"+"[1.5cm]"
-#    start+="\\"+"\\"+"\\"
-
-
-    middle="""
-    \\end{Puzzle}
-
-    \\begin{PuzzleClues}{\\textbf{Hints}}\\\\"""
-
+\\end{document}"""
+    else:   
     end = """\\end{PuzzleClues}
 
             \\end{document}
